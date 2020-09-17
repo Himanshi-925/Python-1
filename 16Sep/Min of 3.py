8623813b3c160b5e6fdb71a6b915b9ee2deabdde
Abhishek <70463156+abh1shekgupta@users.noreply.github.com>
2020-09-17 11:49:03 +0530

    Update Min of 3.py

--- 16Sep/Min of 3.py
+++ 16Sep/Min of 3.py
@@ -1,1 +1,11 @@
+#Minimun of given three number
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+c = int(input("Enter value of c: "))
 
+if a < b and a < c:
+    print(a, 'is smallest among given three numbers.')
+elif b < c:
+    print(b, 'is smallest among given three numbers.')
+else:
+    print(c, 'is smallest among given three numbers.')
