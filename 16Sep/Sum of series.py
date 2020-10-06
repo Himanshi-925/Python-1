b39b51f0e51e443e45bd3abb104bf683f1fe06ac
Himanshi Goyal <71343004+Himanshi-925@users.noreply.github.com>
2020-10-06 20:23:43 +0530

    Update Sum of series.py

--- 16Sep/Sum of series.py
+++ 16Sep/Sum of series.py
@@ -1,8 +1,8 @@
-#To find the sum of series upto given number..
+#To find the sum of series upto given number with step of 2..
 num = int(input('Enter the number upto which u wanna see the sum:'))
-i = 1
-r = 0
-while i <= num:
-    r += i
-    i += 1
-print('The sum upto given number is : ', r)
+initial = 1
+result = 0
+while initial <= num:
+    result += initial
+    initial += 1
+print('The sum upto given number is : ', result)
